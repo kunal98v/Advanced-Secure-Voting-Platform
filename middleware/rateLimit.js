f86233303286cf7233bf4d01815de80582f86c77
kunal98v <kunal98v@gmail.com>
2025-10-26 19:53:02 +0530

    created docker image

--- middleware/rateLimit.js
+++ middleware/rateLimit.js
@@ -1,9 +1,9 @@
-const rateLimit = require("express-rate-limit");
+const {rateLimit, ipKeyGenerator } = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 60*1000, 
   max: 5, 
-  keyGenerator: (req) => req.ip + ":" + req.path,
+  keyGenerator: (req) => ipKeyGenerator(req) + ":" + req.path,
   handler: (req, res, next) => res.status(429).json({'message': 'You have exceeded the request limit. Try again later.'})
 });
 
